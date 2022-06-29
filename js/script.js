574d381de869f43452705619eb244b52929a6628
E-C-Shackelford <ecs.shackelford@gmail.com>
2022-06-28 22:12:16 -0700

    Add an array of to-do elements, a function, and an event listener so that when the Show List button is clicked the to-do list displays and the Hide List button makes the list disappear when clicked

--- js/script.js
+++ js/script.js
@@ -1,1 +1,37 @@
+const todos = [
+  "Feed the dogs",
+  "Water indoor plants",
+  "Do laundry",
+  "JavaScript studies",
+  "Take the dogs out to exercise",
+  "Workout",
+  "Code a small project",
+  "Water outdoor plants",
+  "Garden",
+  "Work on art piece",
+  "Meditate",
+  "Vacuum and mop floors",
+];
 
+const button = document.querySelector(".show-todos");
+const todoList = document.querySelector(".todos");
+
+const showTodos = function () {
+  todos.forEach(function (todo) {
+    const li = document.createElement("li");
+    li.innerHTML = todo;
+    todoList.append(li);
+  });
+  todoList.classList.remove("hide");
+  button.innerText = "Hide List";
+};
+
+button.addEventListener("click", function () {
+  if (todoList.classList.contains("hide")) {
+    showTodos();
+  } else {
+    button.innerText = "Show list!";
+    todoList.classList.add("hide");
+    todoList.innerHTML = "";
+  }
+});
